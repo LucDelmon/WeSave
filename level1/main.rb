d299f38679448d5d91db8dfa840dbcc2360d7aea
LucDelmon <35261367+LucDelmon@users.noreply.github.com>
2021-09-28 19:44:43 +0200

    feat: add the main file
    adding the main file calling the rental service on a test file for the purpose of this exercise.

---
+++ level1/main.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'date'
+require_relative 'lib/services/rental_service'
+
+input_file = File.read('./data/input.json')
+rental_service = RentalService.new(input_file)
+computed_rentals = rental_service.call
+
+puts JSON.pretty_generate({ "rentals": computed_rentals })
+
+
+
