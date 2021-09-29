47ad48413838630dfbe7c5825f82992136dea5cc
LucDelmon <35261367+LucDelmon@users.noreply.github.com>
2021-09-29 12:21:34 +0200

    refacto: rental_service

--- level3/lib/services/rental_service.rb
+++ level3/lib/services/rental_service.rb
@@ -4,39 +4,57 @@
 #
 # Compute rental prices
 class RentalService
-    def initialize(input_file)
-      input = JSON.parse(input_file)
-      @cars = input["cars"].map { |c| [c['id'], c] }.to_h
-      @rentals = input["rentals"]
-    end
+  FIRST_DISCOUNT = 0.9
+  SECOND_DISCOUNT = 0.7
+  THIRD_DISCOUNT = 0.5
 
-    # @return [Array]
-    def call
-      @rentals.map do |rental| 
-        car = @cars[rental['car_id']]
-        { id: rental['id'], price: rental_price(car, rental) }
-      end
+  # @param input_file [String]
+  def initialize(input_file)
+    input = JSON.parse(input_file)
+    @cars = input["cars"].map { |c| [c['id'], c] }.to_h
+    @rentals = input["rentals"]
+  end
+
+  # @return [Array]
+  def call
+    @rentals.map do |rental| 
+      car = @cars[rental['car_id']]
+      { id: rental['id'], price: rental_price(car: car, rental: rental) }
     end
+  end
 
   private
 
+
+  # @param car [Hash]
+  # @param rental [Hash]
   # @return [Integer]
-  def rental_price(car, rental)
-    rental_duration = (Date.parse(rental['end_date']) - Date.parse(rental['start_date'])).to_i + 1
-    duration_price(car, rental_duration) + rental['distance'] * car['price_per_km']
+  def rental_price(car:, rental:)
+    duration_price(car: car, rental: rental) + distance_price(car: car, rental: rental)
   end
 
+  # @param car [Hash]
+  # @param rental [Hash]
   # @return [Integer]
-  def duration_price(car, rental_duration)
+  def distance_price(car:, rental:)
+    rental['distance'] * car['price_per_km']
+  end
+
+
+  # @param car [Hash]
+  # @param rental [Hash]
+  # @return [Integer]
+  def duration_price(car:, rental:)
+    rental_duration = (Date.parse(rental['end_date']) - Date.parse(rental['start_date'])).to_i + 1
     price_factor = case rental_duration 
       when 1
         1
       when 2..4
-        1 + 0.9*(rental_duration-1)
+        1 + FIRST_DISCOUNT*(rental_duration-1)
       when 5..10
-        1 + 0.9*3 + 0.7*(rental_duration-4)
+        1 + FIRST_DISCOUNT*3 + SECOND_DISCOUNT*(rental_duration-4)
       when 10..Float::INFINITY
-        1 + 0.9*3 + 0.7*6 + 0.5*(rental_duration-10)
+        1 + FIRST_DISCOUNT*3 + SECOND_DISCOUNT*6 + THIRD_DISCOUNT*(rental_duration-10)
     end
     (car['price_per_day'] * price_factor).round(half: :up) 
   end
