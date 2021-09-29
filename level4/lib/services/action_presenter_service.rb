c30684c1dfc1cf11b3a5568c2bceda36b249b776
LucDelmon <35261367+LucDelmon@users.noreply.github.com>
2021-09-29 13:48:29 +0200

    feat: Create action_presenter_service
    New service generating a list of debit/credit actions for each rental infos generated by the rental service

---
+++ level4/lib/services/action_presenter_service.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+# class ActionPresenterService
+#
+# Present an hash of detailled rental as a list of debit/credit actions
+class ActionPresenterService
+
+  # @param computed_rentals [Hash]
+  def initialize(computed_rentals)
+    @rentals = computed_rentals
+  end
+
+  # @return [Array]
+  def call
+    @rentals.map do |rental| 
+      { id: rental[:id], actions: actions_list(rental) }
+    end
+  end
+  
+  private
+
+  # @return [Array]
+  def actions_list(rental)
+    [
+      add_debit(who: 'driver', amout: rental[:price]),
+      add_credit(who: 'owner', amout: rental[:owner_part]),
+      add_credit(who: 'insurance', amout: rental[:commission][:insurance_fee]),
+      add_credit(who: 'assistance', amout: rental[:commission][:assistance_fee]),
+      add_credit(who: 'drivy', amout: rental[:commission][:drivy_fee]),
+    ]
+  end
+
+  def add_credit(who:, amout:)
+    { 'who': who, 'type': 'credit', 'amount': amout }
+  end
+
+  def add_debit(who:, amout:)
+    { 'who': who, 'type': 'debit', 'amount': amout }
+  end
+end
+
